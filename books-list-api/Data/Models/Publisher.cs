b4a05bcc85e0a3741aaff6cb58549b953bab580c
ganesh pawar <ganeshpawar3444@gmail.com>
2025-01-14 05:44:47 +0530

    added OneToManyRelationship

---
+++ books-list-api/Data/Models/Publisher.cs
@@ -0,0 +1,12 @@
+﻿namespace books_list_api.Data.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Navigation properties
+
+        public List<Book> Books { get; set; }
+    }
+}
