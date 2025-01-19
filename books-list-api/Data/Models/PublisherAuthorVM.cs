173aede2c340e47fecb54c260c1058943b9c641b
ganesh pawar <ganeshpawar3444@gmail.com>
2025-01-19 12:01:38 +0530

    Added get pusblisher with book and author apis

---
+++ books-list-api/Data/Models/PublisherAuthorVM.cs
@@ -0,0 +1,10 @@
+﻿namespace books_list_api.Data.Models
+{
+    public class PublisherAuthorVM
+    {
+        public int PublisherId { get; set; }
+        public string PublisherName { get; set; }
+
+        public List<IEnumerable<string>> Authors { get; set; }
+    }
+}
