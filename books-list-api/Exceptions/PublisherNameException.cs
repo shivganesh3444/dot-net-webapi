8598f62e3fae3e74d72275c55260d76ea78781ec
ganesh pawar <ganeshpawar3444@gmail.com>
2025-01-20 19:24:13 +0530

    added custom exception handling for publisher name

---
+++ books-list-api/Exceptions/PublisherNameException.cs
@@ -0,0 +1,23 @@
+﻿namespace books_list_api.Exceptions
+{
+    public class PublisherNameException : Exception
+    {
+        public readonly string _publisherName = string.Empty;
+        public PublisherNameException()
+        {
+            
+        }
+        public PublisherNameException(string message) : base(message)
+        {
+
+        }
+        public PublisherNameException(string message, Exception ex) : base(message, ex)
+        {
+
+        }
+        public PublisherNameException(string message, string publisherName) : base(message)
+        {
+            _publisherName = publisherName;
+        }
+    }
+}
